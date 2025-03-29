153c74a315a0e7b5111201e477a80848ac2e1437
Mohammad <ma@bornak.de>
2025-03-29 23:23:10 +0330

    Initial Commit

---
+++ CustomButtonStyle/CustomButtonStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButtonStyleApp.swift
+//  CustomButtonStyle
+//
+//  Created by Mohammad on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomButtonStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
