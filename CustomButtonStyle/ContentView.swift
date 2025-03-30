2747d1ba758b8a6983c6fe307c74401567ef7a8f
Mohammad <ma@bornak.de>
2025-03-30 17:37:51 +0330

    Added shadow on code

--- CustomButtonStyle/ContentView.swift
+++ CustomButtonStyle/ContentView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+import SwiftUI
+
 enum ButtonStyleConstant {
     static let padding: CGFloat = 15
 }
@@ -16,23 +18,23 @@ struct SocialButtonSetting {
     var color: Color
 }
 
-extension SocialButtonSetting: Equatable {} // for test equal
+extension SocialButtonSetting: Equatable {}
 
-struct SocailStyleEnvironmentKey: EnvironmentKey {
+struct SocialStyleEnvironmentKey: EnvironmentKey {
     static var defaultValue: SocialButtonSetting = .init(iconName: "questionmark.circle", color: .clear)
 }
 
 extension EnvironmentValues {
     var socialStyle: SocialButtonSetting {
-        get { self[SocailStyleEnvironmentKey.self] }
-        set { self[SocailStyleEnvironmentKey.self] = newValue }
+        get { self[SocialStyleEnvironmentKey.self] }
+        set { self[SocialStyleEnvironmentKey.self] = newValue }
     }
 }
 
-struct SocailButtonViewModefier: ViewModifier {
+struct SocialButtonViewModifier: ViewModifier {
     let isPressed: Bool
-    
     @Environment(\.socialStyle) var style
+    
     func body(content: Content) -> some View {
         HStack {
             Image(systemName: style.iconName)
@@ -43,25 +45,26 @@ struct SocailButtonViewModefier: ViewModifier {
                 .padding(.vertical, ButtonStyleConstant.padding)
                 .padding(.trailing, ButtonStyleConstant.padding)
         }
-        .background(style.color.gradient)
+        .background(style.color)
         .cornerRadius(8)
-        .scaleEffect(isPressed ? 0.9 : 1)
-        .animation(.smooth, value: isPressed)
+        .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 4)
+        .scaleEffect(isPressed ? 0.95 : 1)
+        .animation(.spring(response: 0.3, dampingFraction: 0.7), value: isPressed)
         .frame(minWidth: 200, maxWidth: .infinity)
         .fixedSize(horizontal: false, vertical: true)
         .dynamicTypeSize(.xSmall ... .xxLarge)
     }
 }
 
-struct SocailButtonStyle: ButtonStyle {
+struct SocialButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
-        
-        configuration.label.modifier(SocailButtonViewModefier(isPressed: configuration.isPressed))
+        configuration.label
+            .modifier(SocialButtonViewModifier(isPressed: configuration.isPressed))
     }
 }
 
-extension ButtonStyle where Self == SocailButtonStyle {
-    static var social: SocailButtonStyle { .init() }
+extension ButtonStyle where Self == SocialButtonStyle {
+    static var social: SocialButtonStyle { .init() }
 }
 
 extension View {
@@ -69,28 +72,21 @@ extension View {
         environment(\.socialStyle, style)
     }
 }
+
 struct ContentView: View {
-    
     var body: some View {
         VStack(spacing: 10) {
-            Button("Sign in with Apple") {
-                
-            }
-            .socialStyle(.init(iconName: "apple.logo", color: .black))
-            .buttonStyle(.social)
+            Button("Sign in with Apple") {}
+                .socialStyle(.init(iconName: "apple.logo", color: .black))
+                .buttonStyle(.social)
             
+            Button("Sign in with Google") {}
+                .socialStyle(.init(iconName: "g.circle.fill", color: .red))
+                .buttonStyle(.social)
             
-            Button("Sign in with Google") {
-                
-            }
-            .socialStyle(.init(iconName: "g.circle.fill", color: .red))
-            .buttonStyle(.social)
-            
-            Button("Sign in with Facebook") {
-                
-            }
-            .socialStyle(.init(iconName: "f.circle.fill", color: .blue))
-            .buttonStyle(.social)
+            Button("Sign in with Facebook") {}
+                .socialStyle(.init(iconName: "f.circle.fill", color: .blue))
+                .buttonStyle(.social)
         }
         .foregroundStyle(.white)
         .padding()
