92e732db1d84015b28c139adcab20c647e90c94c
Mohammad Akbari <makbariengineer@gmail.com>
2025-10-06 18:37:47 +0330

    Update And work ui

---
+++ CustomButtonStyle/CustomCircleButtons.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCircleButtons.swift
+//  CustomButtonStyle
+//
+//  Created by Mohammad on 4/1/25.
+//
+
+import SwiftUI
+
+struct CustomCircleButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomCircleButtons()
+}
