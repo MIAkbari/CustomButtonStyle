035974516fbb2b2acecafc0e79ee099af8ad1170
Mohammad Akbari <makbariengineer@gmail.com>
2025-10-08 23:41:38 +0330

    # Added code to update new button view

---
+++ CustomButtonStyle/AppButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  AppButtonView.swift
+//  CustomButtonStyle
+//
+//  Created by Mohammad on 10/8/25.
+//
+
+import SwiftUI
+
+struct AppButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppButtonView()
+}
